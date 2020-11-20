1c6f14d8c2f4e32348df88a03eef43dbb5f35797
L4zyy <turt1e@foxmail.com>
2020-11-21 00:04:59 +0800

    add self attention demo model.

--- lib/models/vibe.py
+++ lib/models/vibe.py
@@ -212,3 +212,66 @@ class VIBE_Demo(nn.Module):
             s['rotmat'] = s['rotmat'].reshape(batch_size, seqlen, -1, 3, 3)
 
         return smpl_output
+
+class SA_VIBE_Demo(nn.Module):
+    def __init__(
+            self,
+            seqlen,
+            batch_size=64,
+            n_layers=1,
+            hidden_size=2048,
+            add_linear=False,
+            bidirectional=False,
+            use_residual=True,
+            pretrained=osp.join(VIBE_DATA_DIR, 'spin_model_checkpoint.pth.tar'),
+            temporal_type='gru',
+            n_head=0,
+    ):
+
+        super(SA_VIBE_Demo, self).__init__()
+
+        self.seqlen = seqlen
+        self.batch_size = batch_size
+
+        self.encoder = TemporalEncoder(
+            n_layers=n_layers,
+            hidden_size=hidden_size,
+            bidirectional=bidirectional,
+            add_linear=add_linear,
+            use_residual=use_residual,
+            temporal_type=temporal_type,
+            seqlen=self.seqlen,
+            n_head=n_head,
+        )
+
+        self.hmr = hmr()
+        checkpoint = torch.load(pretrained)
+        self.hmr.load_state_dict(checkpoint['model'], strict=False)
+
+        # regressor can predict cam, pose and shape params in an iterative way
+        self.regressor = Regressor()
+
+        if pretrained and os.path.isfile(pretrained):
+            pretrained_dict = torch.load(pretrained)['model']
+
+            self.regressor.load_state_dict(pretrained_dict, strict=False)
+            print(f'=> loaded pretrained model from \'{pretrained}\'')
+
+
+    def forward(self, input, J_regressor=None):
+        # input size NTF
+        batch_size, seqlen, nc, h, w = input.shape
+
+        feature = self.hmr.feature_extractor(input.reshape(-1, nc, h, w))
+        feature = self.encoder(feature)
+        feature = feature.reshape(-1, feature.size(-1))
+
+        smpl_output = self.regressor(feature, J_regressor=J_regressor)
+        for s in smpl_output:
+            s['theta'] = s['theta'].reshape(batch_size, seqlen, -1)
+            s['verts'] = s['verts'].reshape(batch_size, seqlen, -1, 3)
+            s['kp_2d'] = s['kp_2d'].reshape(batch_size, seqlen, -1, 2)
+            s['kp_3d'] = s['kp_3d'].reshape(batch_size, seqlen, -1, 3)
+            s['rotmat'] = s['rotmat'].reshape(batch_size, seqlen, -1, 3, 3)
+
+        return smpl_output
